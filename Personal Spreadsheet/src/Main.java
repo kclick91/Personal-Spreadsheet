4d391bb7dae843e876751b2c5741edc7ddc5f179
Kelly Click <kclick91@gmail.com>
2024-08-27 17:50:27 -0400

    Simple demonstration

--- Personal Spreadsheet/src/Main.java
+++ Personal Spreadsheet/src/Main.java
@@ -6,32 +6,60 @@ public class Main {
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Welcome to my Personal Spreadsheets");
 
-        Spreadsheet spsh = new Spreadsheet(100, 100);
+        Spreadsheet spsh = new Spreadsheet(100, 100, "Spreadsheet of Classes");
+        Spreadsheet spshTwo = new Spreadsheet(50,50, "Answer Keys");
 
         double[][] testValues = new double[][]{{68,75,88},{99,77,71},{89,88,81}};
 
+        double[][] testValuesTwo = new double[][]{{87,88,87},{83,89,83},{97,90,89}};
+
+        double[][] testAnswers = new double[][]{{4.1, 5.1, 4.3, 4.7, 10.2},{4.1, 5.9, 2.3, 4.7, 11.2}, {1.5, 60.1, 4.3, 4.7, 10.2}, {4.1, 15.1, 40.3, 14.7, 10.2}, {4.1, 5.1, 4.3, 6.7, 12.2}};
         Table t = new Table("B", 2, 3, 3,testValues);
+        Table tTwo = new Table("B", 6, 4,4, testValuesTwo);
+
+        Table tThree = new Table("B", 2, 5, 4, testAnswers);
+
 
         spsh.AddTable(t);
+        spshTwo.AddTable(tThree);
 
+        //Table 1
         //Cells A2 through A4
         RowColumnNames rcnOne = new RowColumnNames(new String[]{"Student 1", "Student 2", "Student 3"}, 3);
         //Cells B1 through D1
         RowColumnNames rcnTwo = new RowColumnNames(new String[]{"Grade 1", "Grade 2", "Grade 3"}, 3);
 
+
+
         //Set A1 through D4, Show A1 through D4
 
         //Temporary display for example, needs to be saved to cells
+        System.out.println(spsh.GetName());
         System.out.println("---- " + rcnTwo.names[0] + " " + rcnTwo.names[1] + " " + rcnTwo.names[2]);
         System.out.println(rcnOne.names[0] + " "  + t.GetValues()[0][0] + " " + t.GetValues()[0][1] + " " + t.GetValues()[0][2]);
         System.out.println(rcnOne.names[1] + " "  + t.GetValues()[1][0] + " " + t.GetValues()[1][1] + " " + t.GetValues()[1][2]);
         System.out.println(rcnOne.names[2] + " "  + t.GetValues()[2][0] + " " + t.GetValues()[2][1] + " " + t.GetValues()[2][2]);
 
+        //System.out.println(spsh.GetName());
+        System.out.println("---- " + rcnTwo.names[0] + " " + rcnTwo.names[1] + " " + rcnTwo.names[2]);
+        System.out.println(rcnOne.names[0] + " "  + tTwo.GetValues()[0][0] + " " + tTwo.GetValues()[0][1] + " " + tTwo.GetValues()[0][2]);
+        System.out.println(rcnOne.names[1] + " "  + tTwo.GetValues()[1][0] + " " + tTwo.GetValues()[1][1] + " " + tTwo.GetValues()[1][2]);
+        System.out.println(rcnOne.names[2] + " "  + tTwo.GetValues()[2][0] + " " + tTwo.GetValues()[2][1] + " " + tTwo.GetValues()[2][2]);
 
+        //Table Two
+        //Cells A2 through A6
+        RowColumnNames tests = new RowColumnNames(new String[]{"Test 1", "Test 2", "Test 3", "Test 4", "Test 5"}, 5);
 
+        //Cells B2 through B11
+        RowColumnNames answers = new RowColumnNames(new String[]{"Answer 1", "Answer 2", "Answer 3", "Answer 4"}, 4);
 
-
-
+        //Temporary display for example, needs to be saved to cells
+        System.out.println(spshTwo.GetName());
+        System.out.println("---- " + tests.names[0] + " " + tests.names[1] + " " + tests.names[2] + " " + tests.names[3] + " " + tests.names[4]);
+        System.out.println(answers.names[0] + " "  + tThree.GetValues()[0][0] + " " + tThree.GetValues()[0][1] + " " + tThree.GetValues()[0][2] + " " + tThree.GetValues()[0][3] + " " + tThree.GetValues()[0][4]);
+        System.out.println(answers.names[1] + " "  + tThree.GetValues()[1][0] + " " + tThree.GetValues()[1][1] + " " + tThree.GetValues()[1][2] + " " + tThree.GetValues()[1][3] + " " + tThree.GetValues()[1][4]);
+        System.out.println(answers.names[2] + " "  + tThree.GetValues()[2][0] + " " + tThree.GetValues()[2][1] + " " + tThree.GetValues()[2][2] + " " + tThree.GetValues()[2][3] + " " + tThree.GetValues()[0][4]);
+        System.out.println(answers.names[3] + " "  + tThree.GetValues()[3][0] + " " + tThree.GetValues()[3][1] + " " + tThree.GetValues()[3][2] + " " + tThree.GetValues()[3][3] + " " + tThree.GetValues()[0][4]);
 
 
 
--- Personal Spreadsheet/src/Spreadsheet.java
+++ Personal Spreadsheet/src/Spreadsheet.java
@@ -6,22 +6,39 @@ public class Spreadsheet {
     private int letterSize;
     private int numberSize;
     private List<Table> tableList;
+    private String nameOfSpreadsheet;
+    private Object[][] cells;
     public Spreadsheet()
     {
         tableList = new ArrayList<Table>();
     }
-    public Spreadsheet(int lS, int nS)
+    public Spreadsheet(int lS, int nS, String nSS)
     {
         tableList = new ArrayList<Table>();
         letterSize = lS;
         numberSize = nS;
+        nameOfSpreadsheet = nSS;
+    }
+    public Spreadsheet(int lS, int nS, String nSS, Object[][] c)
+    {
+        tableList = new ArrayList<Table>();
+        letterSize = lS;
+        numberSize = nS;
+        nameOfSpreadsheet = nSS;
+        cells = c;
     }
 
     public void AddTable(Table t)
     {
         tableList.add(t);
-        System.out.println("Wonderful! The table has been added the spreadsheet!");
+        System.out.println("Wonderful! The table has been added to the spreadsheet!");
     }
+
+    public String GetName()
+    {
+        return nameOfSpreadsheet;
+    }
+
 
 
 }
